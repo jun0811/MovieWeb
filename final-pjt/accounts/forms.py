f318557b1f556d638cb45712be74e109c037faa1
itoggi03 <itoggi03@gmail.com>
2020-11-20 11:25:55 +0900

    complete accounts

--- final-pjt/accounts/forms.py
+++ final-pjt/accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 
@@ -7,3 +7,10 @@ class CustomUserCrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'first_name', 'last_name',)
--- final-pjt/accounts/views.py
+++ final-pjt/accounts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
-from .forms import CustomUserCrationForm
+from django.views.decorators.http import require_POST, require_http_methods
+
+from .forms import CustomUserCrationForm, CustomUserChangeForm
 
 # Create your views here.
+require_http_methods(['GET', 'POST'])
 def signup(request):
     # if request.user.is_authenticated:
     #     return redirect('')
@@ -15,7 +21,7 @@ def signup(request):
             # user = form.save()
             # auth
             form.save()
-            return redirect('accounts:signup')
+            return redirect('accounts:signup')  # 수정 필요
     else:
         form = CustomUserCrationForm()
     context = {
@@ -24,6 +30,7 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
+@require_http_methods(['GET', 'POST'])
 def login(request):
     # if request.user.is_authenticated:
     #     return redirect('')
@@ -31,10 +38,56 @@ def login(request):
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect(request.GET.get('next') or 'accounts:login') # or 'movies:index'
+            return redirect(request.GET.get('next') or 'accounts:login') # or 'movies:index'로 수정 필요
     else:
         form = AuthenticationForm()
     context = {
         'form': form,
     }
     return render(request, 'accounts/login.html', context)
+
+
+# @require_POST
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:update')  # 수정 필요
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+    return redirect('accounts:signup') # 수정 필요
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('accounts:update') # 수정 필요
+    else: 
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
