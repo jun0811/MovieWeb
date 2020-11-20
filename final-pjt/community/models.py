287cef8f0366702cbe958f8b1a2e9e2f33acf234
nate1994 <nate199458@gmail.com>
2020-11-20 10:07:26 +0900

    basehtml

--- final-pjt/community/models.py
+++ final-pjt/community/models.py
@@ -10,9 +10,12 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     content = models.TextField()
     review = models.ForeignKey(Review,on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.content
