5c0933726282fd1f8fe14668bc07013430943da1
nate1994 <nate199458@gmail.com>
2020-11-26 11:48:41 +0900

    modify

--- final-pjt/community/urls.py
+++ final-pjt/community/urls.py
@@ -6,10 +6,11 @@ app_name = "community"
 urlpatterns = [
     path('',views.index, name="index"),
     path('reviews/',views.reviews, name="reviews"), 
-    path('create/',views.create, name="create"),
-    path('<int:pk>/',views.detail, name="detail"),
-    path('<int:pk>/update',views.update, name="update"),
-    path('<int:pk>/delete',views.delete, name="delete"),
+    path('create/<movie_id>/',views.create, name="create"),
+    path('<int:pk>/<movie_title>/',views.detail, name="detail"),
+    path('<int:pk>/update/',views.update, name="update"),
+    path('<int:pk>/delete/',views.delete, name="delete"),
     path('<int:pk>/comments/', views.comments_create, name='comments_create'),
     path('<int:review_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('search_title/<str:title>/', views.search_title, name= "search_title"),
 ]
--- final-pjt/community/views.py
+++ final-pjt/community/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Review,Comment
 from django.views.decorators.http import require_http_methods,require_POST
 from .forms import ReviewForm, CommentForm
+from movies.models import Movies
+from django.http import JsonResponse
+
+
 # Create your views here.
 def index(request):
     reviews = Review.objects.order_by('-pk')[0:5]
@@ -21,15 +25,19 @@ def reviews(request):
 
 # @login_required
 @require_http_methods(['GET', 'POST'])
-def create(request):
+def create(request,movie_id):
+    movie = get_object_or_404(Movies,movie_id=movie_id)
+
     if request.method =='POST':
+        print(movie.title)
+        print(request.POST)
         form = ReviewForm(request.POST)
-        print(form)
+        # print(form)
         if form.is_valid():
             review = form.save(commit=False) # 당장 저장하지 않고 user 등록후 저장 
             review.user = request.user # accounts기능 완료후 필요 
             review.save()
-            ## 데이터 모델링에서 user를 넣어놔서 
+            # 데이터 모델링에서 user를 넣어놔서 
             # user필드가 비게되면 오류가 뜨기 때문에 accounts기능을 먼저 
             # 구현하거나 모델링을 수정해가면 할 필요가 있음...
             return redirect('community:index') # 추후에 detail로 수정필요 
@@ -37,18 +45,21 @@ def create(request):
         form = ReviewForm()
     context = {
         'form' : form ,
+        'movie' : movie,
     }
     return render(request,'community/create.html',context)
 
 
-def detail(request, pk):
+def detail(request, pk ,movie_title):
     review = get_object_or_404(Review, pk=pk)
+    movie = Movies.objects.get(title=movie_title)
     comment_form = CommentForm()
     comments = review.comment_set.all()
     context = {
         'review': review,
         'comment_form': comment_form,
         'comments': comments,
+        'movie' : movie,
     }
     return render(request, 'community/detail.html', context)
 
@@ -94,7 +105,6 @@ def comments_create(request,pk):
             comment.review = review
             comment.user = request.user
             comment.save()
-            print('&&&&',review.pk)
             return redirect('community:detail', review.pk)
         context = {
             'comment_form' : comment_form,
@@ -104,10 +114,24 @@ def comments_create(request,pk):
     return redirect('accounts:login')
 
 
-
+@require_POST
 def comments_delete(request, review_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+
     if request.user.is_authenticated:
         comment = get_object_or_404(Comment, pk = comment_pk)
         if request.user == comment.user:
             comment.delete()
     return redirect('community:detail', review_pk)
+
+
+# @require_POST
+def search_title(request, title):
+    print(title)
+    movies = Movies.objects.filter(title__contains=title)
+    # print(movies.object.all())
+    data = {
+        "movies" :movies
+    }
+    return JsonResponse(list(movies.values()),safe=False)
+
